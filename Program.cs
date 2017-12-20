2cdeaf10443d9dc0b987b8684dba8c23b26396ed
JesusLinares01 <34040095+JesusLinares01@users.noreply.github.com>
2017-12-20 11:52:10 -0600

    Total del precio

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Operator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            double mealCost;
+            int tipPercent;
+            int taxPercent;
+            double totalCost;
+
+            mealCost = Convert.ToDouble(Console.ReadLine());
+            tipPercent = Convert.ToInt32(Console.ReadLine());
+            taxPercent = Convert.ToInt32(Console.ReadLine());
+
+            var tip = mealCost * tipPercent / 100;
+            var tax = mealCost * taxPercent / 100;
+            totalCost = mealCost + tip + tax;
+            //var round = Math.Round(totalCost);
+
+
+            Console.WriteLine("The total meal cost is " + Math.Round(totalCost) + " dollars.");
+            Console.ReadKey();
+        }
+    }
+}
